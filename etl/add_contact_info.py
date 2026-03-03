5fe52f3a0a91cacbe0a92ce12059a67627c016c9
Vinicius <vinipontarolla@gmail.com>
2026-03-03 12:44:22 +0100

    Moved logic inside a function and added main() to improve reuse

--- etl/add_contact_info.py
+++ etl/add_contact_info.py
@@ -1,13 +1,20 @@
 import sqlite3
 
-try:
-    con = sqlite3.connect("data/events.db") # Connect to the SQLite database
-    print("Database connection successful")
-    cur = con.cursor() # Create a cursor to execute SQL commands
-    cur.execute("SELECT * FROM enriched_events LIMIT 5") # Execute a sample query
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-except sqlite3.Error as e: # Handle connection errors
-    print(f"Database connection failed: {e}")
-    exit(1)
+def add_contact_info(db_path):
+    try:
+        con = sqlite3.connect("data/events.db") # Connect to the SQLite database
+        print("Database connection successful")
+        cur = con.cursor() # Create a cursor to execute SQL commands
+        cur.execute("SELECT * FROM enriched_events LIMIT 5") # Execute a sample query
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+    except sqlite3.Error as e: # Handle connection errors
+        print(f"Database connection failed: {e}")
+        exit(1)
+
+def main():
+    add_contact_info("data/events.db")
+
+if __name__ == "__main__":
+    main()
