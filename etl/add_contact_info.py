f82c2d2441b1f5cf5dffb7f5385affe077221bc9
Vinicius <vinipontarolla@gmail.com>
2026-03-03 14:32:13 +0100

    Add pandas for dataframe manipulation; Add context manager to handle database connection; Add logic to load data from json, combine with data from enriched events and generate a new dataframe

--- etl/add_contact_info.py
+++ etl/add_contact_info.py
@@ -1,17 +1,18 @@
 import sqlite3
+import pandas as pd
 
 def add_contact_info(db_path):
-    try:
-        con = sqlite3.connect("data/events.db") # Connect to the SQLite database
-        print("Database connection successful")
-        cur = con.cursor() # Create a cursor to execute SQL commands
-        cur.execute("SELECT * FROM enriched_events LIMIT 5") # Execute a sample query
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-    except sqlite3.Error as e: # Handle connection errors
-        print(f"Database connection failed: {e}")
-        exit(1)
+    with sqlite3.connect(db_path) as con:
+        try:
+            print("Database connection successful")
+            df = pd.read_sql_query("SELECT DISTINCT user_id, user_email, loyalty_status, country FROM enriched_events WHERE user_id IS NOT NULL", con) # Test query to verify connection
+            contact_info = pd.read_json("data/user_contact_details.json") # Load contact info from JSON file
+            contact_info = contact_info.rename(columns={"id": "user_id"}) # Rename 'id' to 'user_id' for merging
+            df = df.merge(contact_info, left_on="user_id", right_on="user_id", how="left")
+            print(df)
+        except sqlite3.Error as e: # Handle connection errors
+            print(f"Database connection failed: {e}")
+            exit(1)
 
 def main():
     add_contact_info("data/events.db")
